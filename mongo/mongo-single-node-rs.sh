5ea15767a8d580097702e4cda57fb9bdd5369673
Mokshesh Dafariya <mokshesh@outlook.com>
2023-11-27 06:58:57 +0530

    feat: add compose file for mongo replica set with single node

---
+++ mongo/mongo-single-node-rs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ ! -f "./mongo.keyfile" ]; then
+    openssl rand -base64 741 > mongo.keyfile
+fi
+
+DOCKER_COMPOSE_FILE="mongo-compose.yml"
+
+# Set the name of your MongoDB container
+CONTAINER_NAME="mongo"
+
+#Start MongoDB using Docker Compose
+docker compose --file "$DOCKER_COMPOSE_FILE" up --detach --build
+
+#Wait for MongoDB to initialize
+sleep 5
+
+# MongoDB replica set initiation code
+REPLICA_SET_CODE="rs.initiate({
+  _id: 'rs0',
+  members: [
+    { _id: 0, host: 'localhost:27017' }
+  ]
+});"
+
+# Use docker exec to execute the MongoDB shell with the configuration file
+docker exec -it "$CONTAINER_NAME" mongosh -u root -p root --authenticationDatabase admin --eval "$REPLICA_SET_CODE"
+
+# Additional optional step to check the replica set status
+docker exec -it mongo mongosh -u root -p root --authenticationDatabase admin --eval "rs.status()"
